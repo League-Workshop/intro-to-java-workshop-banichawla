0289877e7cf6210ef456ed18334691416101d4cc
League Student <league@iMac-34.attlocal.net>
2019-08-01 14:27:39 -0700

    day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,45 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	Random r=new Random();
+	int change=r.nextInt(4);
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
-
+	System.out.println(change);
 	// 4. Get the user to enter something that they think is awesome
-
+String awsome= JOptionPane.showInputDialog("What do you think is awsome?");
 	// 5. If the random number is 0
-
+if(change==0) {
+JOptionPane.showMessageDialog(null,"That's awsome!");
+}
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
-
+if(change==1) {
+	JOptionPane.showMessageDialog(null,"That's good i guess!");	
+}
 	// -- tell the user whatever they entered is ok.
 
 	// 7. If the random number is 2
-
+if(change==2) {
+	JOptionPane.showMessageDialog(null,"That's sooo boring!");	
+}
 	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
-
+if(change==3) {
+	JOptionPane.showMessageDialog(null,"dude that's dumb!");	
+}
 	// -- write your own answer
-
+       }
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,32 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
+		String football= JOptionPane.showInputDialog("Is Carson Weinz a football player?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(football.equalsIgnoreCase("yes")) {
+			score++;
+		}
+		String cricket= JOptionPane.showInputDialog("Does Arin Ramsey play cricket?");
+		if(cricket.equalsIgnoreCase("no")) {
+			score++;
+		}
+		String basketball= JOptionPane.showInputDialog("Does Stephen Starburg play basketball?");
+		if(cricket.equalsIgnoreCase("no")) {
+			score++;
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		System.out.println(score);
 	}
 }
